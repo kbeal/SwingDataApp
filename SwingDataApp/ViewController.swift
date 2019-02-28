8ec7c6190296f53515b62ad59089c5b58237d668
Kyle Beal <kyledbeal@gmail.com>
2019-02-28 08:50:06 -0500

    Initial Commit

---
+++ SwingDataApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwingDataApp
+//
+//  Created by kylebeal-outlook on 2/28/19.
+//  Copyright © 2019 kylebeal-outlook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
